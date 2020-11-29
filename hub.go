f06fa6d03990dd0e1dd89ac816c18ed88491bbdc
Dustin Lo <dustinlo.pcc@gmail.com>
2020-11-28 22:03:48 -0800

    moved redis subscription inside Hub struct (#6)
    * moved redis subscription inside Hub struct
    
    removed goroutines from Client struct and moved all logic to Hub's select statement
    moved registration logic of Clients & rooms to re-usable functions
    removed sync.DoOnce from Client struct
    added auto scroll to bottom when new messages are sent to browser in index.html
    added cli flag params, ie. debug and redis host string
    
    * edited log statement

--- hub.go
+++ hub.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"sort"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait = time.Millisecond * 100
 )
 
 // Hub is ...
 type Hub struct {
-	register   chan *Client
-	unregister chan *Client
-	rooms      map[string]map[*Client]bool
+	register    chan *Client
+	unregister  chan *Client
+	broadcast   chan *Message
+	rooms       map[string]map[*Client]bool
+	redisClient *redis.Pool
+	psc         *redis.PubSubConn
 }
 
 // Message is ...
@@ -17,54 +30,98 @@ type Message struct {
 	message []byte
 }
 
-func newHub() *Hub {
+func newHub(psc *redis.PubSubConn) *Hub {
 	return &Hub{
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
+		broadcast:  make(chan *Message),
 		rooms:      make(map[string]map[*Client]bool),
+		psc:        psc,
 	}
 }
 
-func (h *Hub) removeUser() {
-	// TODO: MAYBE(?) move user removal logic here
+func (h *Hub) redisListener() { //(ch chan *Message) {
+	for {
+		defer h.psc.Close()
+		switch v := h.psc.Receive().(type) {
+		case redis.Message:
+			if debug == true {
+				log.Println(string(v.Data))
+			}
+			h.broadcast <- &Message{v.Channel, v.Data}
+		case redis.Subscription: // https://godoc.org/github.com/garyburd/redigo/redis#Subscription
+			log.Println(v)
+		case error:
+			log.Printf("redis pubsub receive err: %v\n", v)
+			panic("Redis connection broke")
+		default:
+			log.Println("something else happened")
+		}
+	}
 }
 
-func (h *Hub) run(r *RedisHub, ch chan *Message) {
+func (h *Hub) printRoomsSize() {
+	fmt.Println("####################################")
+	keys := make([]string, 0)
+	for k := range h.rooms {
+		keys = append(keys, k)
+	}
+	if len(keys) > 0 {
+		sort.Strings(keys)
+		for _, k := range keys {
+			fmt.Println(k, len(h.rooms[k]))
+		}
+	} else {
+		fmt.Println("NO MORE CLIENTS IN H.ROOMS")
+	}
+	fmt.Println("####################################")
+}
+
+func (h *Hub) registerUser(c *Client) {
+	for _, room := range c.rooms {
+		if h.rooms[room] == nil {
+			h.rooms[room] = make(map[*Client]bool)
+			h.psc.Subscribe(room)
+		}
+		h.rooms[room][c] = true
+	}
+	log.Println("client registered", c.conn.RemoteAddr())
+	if debug == true {
+		h.printRoomsSize()
+	}
+}
+
+func (h *Hub) unregisterUser(c *Client) {
+	for _, room := range c.rooms {
+		delete(h.rooms[room], c)
+		if h.rooms[room] != nil && len(h.rooms[room]) == 0 {
+			delete(h.rooms, room)
+			h.psc.Unsubscribe(room)
+		}
+		if debug == true {
+			h.printRoomsSize()
+		}
+	}
+	log.Println("client UN-registered", c.conn.RemoteAddr())
+	c.conn.Close()
+}
+
+func (h *Hub) run() { //(ch chan *Message) {
 	for {
 		select {
 		case c := <-h.register:
-			for _, room := range c.rooms {
-				if h.rooms[room] == nil {
-					h.rooms[room] = make(map[*Client]bool)
-					r.subscribe <- room
-				}
-				h.rooms[room][c] = true
-			}
-			log.Println("client registered", c.conn.RemoteAddr())
-
+			h.registerUser(c)
 		case c := <-h.unregister:
-			c.closeOnce.Do(func() {
-				close(c.send)
-			})
-			for _, room := range c.rooms {
-				// maybe move the deletion of users in an outside re-usable function
-				delete(h.rooms[room], c)
-				if h.rooms[room] != nil && len(h.rooms[room]) == 0 {
-					delete(h.rooms, room)
-					r.unsubscribe <- room
-				}
-			}
-			log.Println("client UN-registered", c.conn.RemoteAddr())
-
-		case msg := <-ch:
-			// log.Println(string(msg.message))
+			h.unregisterUser(c)
+		case msg := <-h.broadcast:
 			for c := range h.rooms[msg.room] {
-				c.send <- msg.message
-				/*
-					TODO:
-					maybe wanna move the send messages here instead
-					if i "un-register" the client outside it would cause a lock
-				*/
+				c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+
+				err := c.conn.WriteMessage(websocket.TextMessage, []byte(msg.message))
+				if err != nil {
+					log.Println("Sent message err: ", err)
+					h.unregisterUser(c)
+				}
 			}
 		}
 	}
